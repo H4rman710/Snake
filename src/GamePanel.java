c666eb1c51f31ac5aa71d42f3b252341bd78aeb4
H4rman <harmansalzbrunn710@gmail.com>
2025-08-13 19:27:55 +0100

    created the body of the snake

--- src/GamePanel.java
+++ src/GamePanel.java
@@ -47,10 +47,21 @@ public class GamePanel extends JPanel implements ActionListener{
         for(int i = 0; i<SCREEN_HEIGHT/UNIT_SIZE; i++){
             g.drawLine(i*UNIT_SIZE,0,i*UNIT_SIZE,SCREEN_HEIGHT);
             g.drawLine(0,i*UNIT_SIZE,SCREEN_WIDTH,i*UNIT_SIZE);
-
         }
         g.setColor(Color.RED);
         g.fillOval(appleX, appleY, UNIT_SIZE, UNIT_SIZE);
+
+        for(int i = 0; i<bodyParts; i++){
+            if(i == 0){
+                g.setColor(Color.GREEN);
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+
+            }
+            else{
+                g.setColor(new Color(45,180,0));
+                g.fillRect(x[i], y[i], UNIT_SIZE, UNIT_SIZE);
+            }
+        }
     }
     public void newApple(){
         appleX = random.nextInt((int)(SCREEN_WIDTH/UNIT_SIZE))*UNIT_SIZE;
